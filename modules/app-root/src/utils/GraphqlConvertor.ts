c841ef30491e17a22f927b47daa0392e1294729f
mrben997 <ngduth97@gmail.com>
2023-10-27 13:35:31 +0700

    fix

--- modules/app-root/src/utils/GraphqlConvertor.ts
+++ modules/app-root/src/utils/GraphqlConvertor.ts
@@ -1,7 +1,6 @@
 class GraphqlConvertorBase {
   fields = ['View', 'Money', 'WatchTime']
   constructor() {}
-  
 }
 const GraphqlConvertor = new GraphqlConvertorBase()
 export default GraphqlConvertor
